d82f9a6369810131a754aee3f91fc9fb6d5778b3
Narcisse Technologies <narcissetechnologies@gmail.com>
2019-12-16 20:46:06 +0100

    [ENH] Improve environment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #coding: utf-8
 
+import sys
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
@@ -7,9 +8,17 @@ import random
 import numpy as np
 
 from src.agent import Agent
-from src.environment import Environment, Status
+from src.environment import Environment
 
 
+def get_free_cell(maze, exclude=[]):
+    free = []
+    for i in range(len(maze)):
+        for j in range(len(maze[i])):
+            if maze[i, j] == 0:
+                if (i, j) not in exclude:
+                    free.append((i, j))
+    return random.choice(free)
 
 if __name__ == "__main__":
     maze = np.array([
@@ -23,28 +32,26 @@ if __name__ == "__main__":
         [0, 0, 0, 0, 0, 1, 0, 0]
     ])
     agent       = Agent(maze.shape[0] * maze.shape[1])
-    environment = Environment(maze)
     episodes    = 100
     loss        = []
     start_list  = list()
     for e in range(episodes):
-        if not start_list:
-            start_list = environment.empty.copy()
-        start_cell = random.choice(start_list)
-        start_list.remove(start_cell)
-        state = environment.reset(start_cell)
+        start_cell = get_free_cell(maze)
+        exit_cell  = get_free_cell(maze, exclude=[start_cell])
+        environment = Environment(maze, start_cell=start_cell, exit_cell=exit_cell)
+        environment.render(block_execution=False)
+        state = environment.reset(start_cell, exit_cell)
         score = 0
         state  = state.flatten()
-        while environment.__status() == Status.PLAYING:
-            #import pdb; pdb.set_trace()
+        max_steps = 100
+        for i in range(max_steps):
             action = agent.act(state)
-            next_state, reward, status = environment.step(action)
+            next_state, reward, done = environment.step(action)
             next_state = next_state.flatten()
-            done = False if status == Status.PLAYING else True
             score += reward
             agent.remember(state, action, reward, next_state, done)
             state = next_state
-            environment.render_q(agent)
+            environment.render()
             agent.replay()
         loss.append(score)
     plt.plot([i for i in range(episodes)], loss)
